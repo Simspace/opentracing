5ed98b3c79bb0357d887a3e73a4c4e8e200f65d1
Matthew Russell <matt@simspace.com>
2017-11-17 18:04:12 -0800

    Open up Sampler

--- opentracing/OpenTracing/Sampling.hs
+++ opentracing/OpenTracing/Sampling.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE ViewPatterns    #-}
 
 module OpenTracing.Sampling
-    ( Sampler(runSampler)
+    ( Sampler(Sampler, runSampler)
     , constSampler
     , probSampler
     , rateLimitSampler
